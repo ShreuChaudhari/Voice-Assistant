a30ebfb0d5314a593308918d312d812ebad02e36
ShreuChaudhari <140592211+ShreuChaudhari@users.noreply.github.com>
2025-03-31 21:22:16 +0530

    Update settings.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -82,5 +82,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:5173",  # The default port for Next.js development
+    "http://localhost:5173", 
+    "https://voice-assistant-production-d04d.up.railway.app",
 ]
